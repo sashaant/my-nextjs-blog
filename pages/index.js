f3408eac0ee7be39373588a4a0459d3c1c7495e6
sashaant <antonenko.qa@gmail.com>
2023-02-27 02:03:04 +0200

    setup skeleton

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,66 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        {/* <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        /> */}
+        <meta name="og:title" content="test" />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to my page!
         </h1>
 
         <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+          Hello, I’m <strong>Sasha</strong>. I’m a software engineer in Test and Developeper.<br/>
+          Making imposible at JABRA_dk<br/>
+          You can contact me on{' '}
+          <a href="https://twitter.com/7antonenko">Twitter</a>.
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="/posts/first-post" className={styles.card}>
+            <h3>First post &rarr;</h3>
+            {/* <p>Find in-depth information about Next.js features and API.</p> */}
+            <p>Check the routiing to the first post page</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="/posts/second-post" className={styles.card}>
+            <h3>Second post &rarr;</h3>
+            <p> Check the routiing to the second post page</p>
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://war.ukraine.ua/support-ukraine/"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h3>I'm standing with the Ukraine &#127482;&#127462; &rarr;</h3>
+            <p>Find out how you can help.</p>
           </a>
+
+       
         </div>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
+          <div>
+          Powered by Sasha Antonenko | 2023
+          </div>
       </footer>
 
       <style jsx>{`
